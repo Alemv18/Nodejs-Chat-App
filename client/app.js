174467bbcf9ee80359306bc9587aafbce652cb63
Alejandra Muñoz <alemv18@gmail.com>
2018-01-28 23:37:10 -0600

    Add initial messages

--- client/app.js
+++ client/app.js
@@ -1,12 +1,22 @@
 var socket = io();
 
-$('form').submit(function () {
-  var text = $('#message').val();
-  socket.emit('message', text);
-  $('#message').val('');
-  return false;
+$('form').on('submit', function() {
+	var text= {
+		message:$('#message').val(), 
+		initials:$('#initials').val()
+	}
+	socket.emit('message', text);
+	$('#message').val('');
+	return false;
 });
 
-socket.on('message', function (msg) {
-  $('<li>').text(msg).appendTo('#history');
+socket.on('message', function(msg) {
+	$('<li>').text(msg).appendTo('#history');
+})
+
+socket.on('history', function(history) {
+	console.log('history');
+	history.forEach(msg => {
+		$('<li>').text(msg).appendTo('#history');
+	});
 });
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ var app = express();
 
 var http = require('http');
 var server = http.Server(app);
+var history = new Array();
 
 app.use(express.static('client'));
 
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
+  socket.emit('history', history);
   socket.on('message', function (msg) {
-    io.emit('message', msg);
+    io.emit('message', `${msg.initials}: ${msg.message}`);
+    history.push(`${msg.initials}: ${msg.message}`);
   });
 });
 
